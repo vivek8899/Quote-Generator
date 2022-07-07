5aeb94af462033da0dc04cc6d978a44dd39ae351
vivek8899 <vivekgp8899@gmail.com>
2022-07-07 17:40:03 +0530

    Add files via upload

---
+++ quote.js
@@ -0,0 +1,55 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function qotday() {
+  window.open("qotd.html" , "_self");
+}
+
+function contact() {
+  window.open("contact.html" , "_self");
+}
+
+function login() {
+  window.open("my-login-master/index.html" , "_self");
+}
+
+function fitquote() {
+  window.open("fitnessQuote/fq.html" ,"_self")
+}
+
+function artquote() {
+  window.open("artQuote/a.html" ,"_self")
+}
+
+function frenquote() {
+  window.open("frenQuote/fren.html" ,"_self")
+}
+
+function lifequote() {
+  window.open("lifeQuote/life.html" ,"_self")
+}
+
+function lovequote() {
+  window.open("loveQuote/love.html" ,"_self")
+}
+
+function motquote() {
+  window.open("motQuote/mot.html" ,"_self")
+}
+
+function relquote() {
+  window.open("relQuote/rel.html" ,"_self")
+}
+
+function petquote() {
+  window.open("petQuote/pet.html" ,"_self")
+}
+
+function famquote() {
+  window.open("famQuote/fam.html" ,"_self")
+}
